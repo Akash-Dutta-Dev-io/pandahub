a2720f31481d2b11e216ef6f1246ed9c9fc9e96a
Akash-Dutta-Dev-io <149801749+Akash-Dutta-Dev-io@users.noreply.github.com>
2023-11-22 18:54:12 +0530

    MODIFIED
    added some new features

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('signup', views.signup, name='registration.html'),
+    path('follow', views.follow, name='follow'),
+    path('search', views.search, name='search'),
     path('profile/<str:pk>', views.profile, name='profile.html'),
     path('upload', views.upload, name='upload'),
     path('like-post', views.like_post, name='like-post'),

5e98315a6104a57dacffb2f87bb7cdd80a8e7579
Shubham Choudhury <shubhamchoudhury2002@yahoo.com>
2023-11-25 22:19:36 +0530

    Bug fixed

--- core/models.py
+++ core/models.py
@@ -5,41 +5,55 @@ from datetime import datetime
 
 User = get_user_model()
 
+
 # Create your models here.
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     id_user = models.IntegerField()
     bio = models.TextField(blank=True)
-    profileimg = models.ImageField(upload_to='profile_images', default='avatar-6.jpg')
+    profileimg = models.ImageField(upload_to="profile_images", default="avatar-6.jpg")
     location = models.CharField(max_length=100, blank=True)
 
+
 def __str__(self):
-       return self.user.username
-   
+    return self.user.username
+
 
 class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     user = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='post_content')
+    image = models.ImageField(upload_to="post_content")
     caption = models.TextField()
     created_at = models.DateTimeField(default=datetime.now)
     no_of_likes = models.IntegerField(default=0)
 
     def __str__(self):
-         return self.user
-         
+        return self.user
+
+    def get_user_profile_pic_url(self):
+        try:
+            # Retrieve the related Profile instance directly through the username
+            profile = Profile.objects.get(user__username=self.user)
+            if profile.profileimg:
+                return profile.profileimg.url
+        except Profile.DoesNotExist:
+            pass
+
+        # Return a default image URL if no profile image is available or profile doesn't exist
+        return "/media/default_profile_image.jpg"
+
 
 class likePost(models.Model):
-     post_id = models.CharField(max_length=500)
-     username = models.CharField(max_length=100)
+    post_id = models.CharField(max_length=500)
+    username = models.CharField(max_length=100)
 
-     def __str__(self):
-          return self.username
-     
+    def __str__(self):
+        return self.username
+
 
 class FollowersCount(models.Model):
-     follower = models.CharField(max_length=100)
-     user = models.CharField(max_length=100)
+    follower = models.CharField(max_length=100)
+    user = models.CharField(max_length=100)
 
-     def __str__(self):
-          return self.user
+    def __str__(self):
+        return self.user
